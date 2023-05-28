d36bcd2b8fd9bc8a200307d226315c1df7c3c464
Kennedy Ekanem <kennedyekanem@gmail.com>
2023-05-28 18:13:36 +0100

    endpoints for status and objs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,73 +1,29 @@
 #!/usr/bin/python3
 """
-    states.py file in v1/views
+This module contains endpoint(route) status
 """
-from flask import abort, Flask, jsonify, request
-from api.v1.views import app_views
 from models import storage
-from models.place import Place
-from models.city import City
-from models.user import User
+from flask import Flask
+from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route("/cities/<city_id>/places", methods=["GET", "POST"], strict_slashes=False)
-def handle_places(city_id):
+@app_views.route('/status', strict_slashes=False)
+def status():
     """
-        Method to return a JSON representation of all states
+    Returns a JSON status
     """
-    city_by_id = storage.get(City, city_id)
-    if city_by_id is None:
-        abort(404, 'Not found')
-
-    if request.method == 'GET':
-        place_list = []
-        for place in city_by_id.places:
-            place_list.append(place.to_dict())
-        return jsonify(place_list)
-
-    elif request.method == 'POST':
-        post = request.get_json()
-        if post is None or type(post) != dict:
-            return jsonify({'error': 'Not a JSON'}), 400
-        elif post.get('name') is None:
-            return jsonify({'error': 'Missing name'}), 400
-        elif post.get('user_id') is None:
-            return jsonify({'error': 'Missing user_id'}), 400
-
-        user_id = post['user_id']
-
-        user_by_id = storage.get(User, user_id)
-        city_by_id = storage.get(City, city_id)
-
-        if not user_by_id or not city_by_id:
-            abort(404)
-
-        new_place = Place(**post)
-        new_place.save()
-        return jsonify(new_place.to_dict()), 201
+    return jsonify({"status": "OK"})
 
 
-@app_views.route("/places/<place_id>", methods=["GET", "PUT", "DELETE"],
-                 strict_slashes=False)
-def handle_place_by_id(place_id):
+@app_views.route('/stats', strict_slashes=False)
+def count():
     """
-        Method to return a JSON representation of a state
+    Retrieves the number of each objects by type
     """
-    place_by_id = storage.get(Place, place_id)
-    if place_by_id is None:
-        abort(404)
-    elif request.method == 'GET':
-        return jsonify(place_by_id.to_dict())
-    elif request.method == 'DELETE':
-        storage.delete(place_by_id)
-        storage.save()
-        return jsonify({}), 200
-    elif request.method == 'PUT':
-        put = request.get_json()
-        if put is None or type(put) != dict:
-            return jsonify({'message': 'Not a JSON'}), 400
-        for key, value in put.items():
-            if key not in ['id', 'created_at', 'updated_at']:
-                setattr(place_by_id, key, value)
-        storage.save()
-        return jsonify(place_by_id.to_dict()), 200
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
